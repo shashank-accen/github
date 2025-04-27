841167d99e837fcc762898979761ed568e9551b3
Shashank <kadiyala.shashank@accenture.com>
2025-04-27 12:09:52 +0530

    Updated the Java file with new changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello, World");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It prints a greeting message to the console.");
     }
 }
