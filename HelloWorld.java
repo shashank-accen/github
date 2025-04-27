d1823d9949359bae531eed11557789bd76a4cfe6
Shashank <kadiyala.shashank@accenture.com>
2025-04-27 12:37:16 +0530

    3rd commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,6 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World");
-        System.out.println("This is a simple Java program.");
-        System.out.println("It prints a greeting message to the console.");
+        System.out.println("This is the third commit");
     }
 }
